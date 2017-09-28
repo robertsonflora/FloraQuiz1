654fc6653affb48c8ed0d46f62c02d2886216bf0
robertsonflora <rpflora@gmail.com>
2017-09-28 16:56:09 +0800

    Initial commit

---
+++ app/src/main/java/com/flora/floraquiz1/Activity/MainActivity.java
@@ -0,0 +1,22 @@
+package com.flora.floraquiz1.Activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.flora.floraquiz1.R;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+        }
+    }
+
